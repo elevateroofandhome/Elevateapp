0d3a453fbcdc4e5b8a1677a6c0d6508920cda82e
elevateroofandhome <elevateroofandhome@gmail.com>
2026-05-01 15:28:03 +1000

    Create app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Elevate Roof & Home Improvements",
+  description: "Roofing and home improvement services in Victoria",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
