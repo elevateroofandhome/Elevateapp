dc762fa5f915bec61a03b5886970efcfe29c6ec3
elevateroofandhome <elevateroofandhome@gmail.com>
2026-05-02 10:29:41 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
 
         <form
           action="https://formspree.io/f/xjglvzde"
-          method="POST"
+          method="post"
           style={{
             display: "flex",
             flexDirection: "column",
